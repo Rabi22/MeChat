4873391a3e386109cbac6670b51bddd786029e12
Rabi22 <rabipradhan320@gmail.com>
2026-06-17 02:48:39 +0530

    v1

---
+++ backend/chat/src/models/Chat.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    users: [{
+      type: String,
+      required: true
+    }],
+    latestMessage: {
+      text: String,
+      sender: String
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Chat = mongoose.model("Chat", schema);
---
+++ backend/chat/src/models/Messages.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    chatId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    sender: {
+      type: String,
+      required: true,
+    },
+    text: String,
+    image: {
+      url: String,
+      publicId: String,
+    },
+    messageType: {
+      type: String,
+      enum: ["text", "image"],
+      default: "text",
+    },
+    seen: {
+      type: Boolean,
+      default: false,
+    },
+    seenAt: {
+      type: Date,
+      default: null,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Messages = mongoose.model("Messages", schema);
